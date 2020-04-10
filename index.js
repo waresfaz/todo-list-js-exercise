793aad8ed5c6f3ed243e65c59591e5f853d0cecd
Wares <fazelyar.wares@gmail.com>
2020-04-10 18:36:59 +0000

    refactored original array based code to OOP

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ function newTask(title, description) {
     markCompleted: function() {
       this.complete = true;
     }
+
   };
   return task;
 }
 
 
-// DRIVER CODE BELOW
+ // DRIVER CODE BELOW
 
-const task1 = newTask("Clean Cat Litter", "Take all the 🟤 out of the box"); // task 0
-const task2 = newTask("Do Laundry", "😰"); // task 1
-const tasks = [task1, task2];
+ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+ const task2 = newTask("Do Laundary", "😨");
+ const tasks = [task1, task2];
 
-task1.logState(); // Clean Cat Litter has not been completed
-task1.markCompleted();
-task1.logState(); // Clean Cat Litter has been completed
+//  logTaskState(task1); // Clean Cat Litter has not been completed
+//  completeTask(task1);
+//  logTaskState(task1); // Clean Cat Litter has been completed
 
-console.log(tasks);
+ console.log(tasks);
